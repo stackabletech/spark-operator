812e9f7add4ab9f18cdf4136262775dead9a0dc0
maltesander <malte.sander.it@gmail.com>
2021-07-21 13:14:25 +0200

    Adapted error handling in generate_crds.

--- crd/src/bin/generate_crds.rs
+++ crd/src/bin/generate_crds.rs
@@ -4,18 +4,32 @@ use stackable_spark_crd::SparkCluster;
 
 fn main() {
     let cluster_target_file = "deploy/crd/sparkcluster.crd.yaml";
-    SparkCluster::write_yaml_schema(cluster_target_file).unwrap();
-    println!("Wrote CRD to [{}]", cluster_target_file);
+    match SparkCluster::write_yaml_schema(cluster_target_file) {
+        Ok(_) => println!("Wrote CRD to [{}]", cluster_target_file),
+        Err(err) => println!(
+            "Could not write CRD to [{}]: {:?}",
+            cluster_target_file, err
+        ),
+    }
 
     let restart_target_file = "deploy/crd/restart.command.crd.yaml";
-    Restart::write_yaml_schema(restart_target_file).unwrap();
-    println!("Wrote CRD to [{}]", restart_target_file);
+    match Restart::write_yaml_schema(restart_target_file) {
+        Ok(_) => println!("Wrote CRD to [{}]", restart_target_file),
+        Err(err) => println!(
+            "Could not write CRD to [{}]: {:?}",
+            restart_target_file, err
+        ),
+    }
 
     let start_target_file = "deploy/crd/start.command.crd.yaml";
-    Start::write_yaml_schema(start_target_file).unwrap();
-    println!("Wrote CRD to [{}]", start_target_file);
+    match Start::write_yaml_schema(start_target_file) {
+        Ok(_) => println!("Wrote CRD to [{}]", start_target_file),
+        Err(err) => println!("Could not write CRD to [{}]: {:?}", start_target_file, err),
+    }
 
     let stop_target_file = "deploy/crd/stop.command.crd.yaml";
-    Stop::write_yaml_schema(stop_target_file).unwrap();
-    println!("Wrote CRD to [{}]", stop_target_file);
+    match Stop::write_yaml_schema(stop_target_file) {
+        Ok(_) => println!("Wrote CRD to [{}]", stop_target_file),
+        Err(err) => println!("Could not write CRD to [{}]: {:?}", stop_target_file, err),
+    }
 }
